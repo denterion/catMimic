cdbbb765d0fcbee782a7415cd5123d28e1ed6437
AndrejeTokyo <centovandrej@gmail.com>
2026-01-11 16:00:40 +0300

    Первое вкл камеры

---
+++ main.py
@@ -0,0 +1,19 @@
+import cv2 as cv
+cap = cv.VideoCapture(0)
+
+if not cap.isOpened():
+    print("Нельзя открыть камеру")
+    exit()
+
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        print("Нельзя получить frame;(")
+        break
+    
+    cv.imshow('Webcam Feed', frame)
+    if cv.waitKey(1) == ord('q'):
+        break
+
+cap.release()
+cv.destroyAllWindows()
