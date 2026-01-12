65a19e23c0febb4d6d6191a0c960a59cf79cdcd0
AndrejeTokyo <centovandrej@gmail.com>
2026-01-12 11:18:13 +0300

    Добавление папки с каскадами, а также фиксирование лица в прямом времени

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import cv2 as cv
 cap = cv.VideoCapture(0)
+face_cascade = cv.CascadeClassifier('cascades/haarcascade_frontalface_default.xml')
 
 
 
@@ -12,7 +13,17 @@ while True:
     if not ret:
         print("Нельзя получить frame;(")
         break
-    
+    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    gray = cv.equalizeHist(gray)
+
+    faces = face_cascade.detectMultiScale(
+        gray,
+        scaleFactor=1.2,
+        minNeighbors=4,
+        minSize=(40, 40)
+    )
+    for (x, y, w, h) in faces:
+        cv.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
     cv.imshow('Webcam Feed', frame)
     if cv.waitKey(1) == ord('q'):
         break
